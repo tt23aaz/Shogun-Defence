32cda02085673d05d7b37052da4e89552d58f2e1
tt23aaz <tt23aaz@herts.ac.uk>
2026-03-19 03:25:39 +0000

    Implemented a healthbar for the enemy
    The health bar to show how much health they have and also added when the player gets a kill they get 5 gold but if the enemy reaches the end they lose 1 health point

--- mobs/enemy_archer.gd
+++ mobs/enemy_archer.gd
@@ -1,12 +1,24 @@
 extends CharacterBody2D
 
-@export var speed = 75
+@export var speed = 100
 var Health = 10
 
 func _process(delta: float) -> void:
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
-	if get_parent().get_progress_ratio() == 1:
-		queue_free()
 	
+	if get_parent().get_progress_ratio() == 1:
+		Game.Health -= 1
+		death()
+		
+		if Game.Health <= 0:
+			get_tree().change_scene_to_file("res://lose_menu.tscn")
+			return
+		
+		
 	if Health <= 0:
-		get_parent().get_parent().queue_free()
+		Game.Gold += 5
+		death()
+	
+
+func death():
+	get_parent().get_parent().queue_free()
