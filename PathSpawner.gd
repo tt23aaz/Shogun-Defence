e37fba2d5cab00ce78863a80807cb67089bd3cfb
tt23aaz <tt23aaz@herts.ac.uk>
2026-03-22 12:24:18 +0000

    Created a temp spawner for the different enemies

--- PathSpawner.gd
+++ PathSpawner.gd
@@ -1,7 +1,29 @@
 extends Node2D
 
-@onready var path = preload("res://mobs/Stage 1.tscn")
+@onready var path_1 = preload("res://mobs/Stage 1.tscn")
+@onready var path_2 = preload("res://mobs/Stage 2.tscn")
+@onready var path_3 = preload("res://mobs/Stage 3.tscn")
+@onready var path_4 = preload("res://mobs/Stage 4.tscn")
 
-func _on_timer_timeout():
-	var tempPath = path.instantiate()
-	add_child(tempPath)
+func _on_timer_timeout() -> void:
+	$Timer.stop()
+
+	for i in range(1):
+		var temp_path = path_1.instantiate()
+		add_child(temp_path)
+		await get_tree().create_timer(3.0).timeout
+
+	for i in range(1):
+		var temp_path_2 = path_2.instantiate()
+		add_child(temp_path_2)
+		await get_tree().create_timer(3.0).timeout
+		
+	for i in range(1):
+		var temp_path_3 = path_3.instantiate()
+		add_child(temp_path_3)
+		await get_tree().create_timer(3.0).timeout
+		
+	for i in range(1):
+		var temp_path_4 = path_4.instantiate()
+		add_child(temp_path_4)
+		await get_tree().create_timer(3.0).timeout
