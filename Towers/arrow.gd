027e61147a240f9afcca4f43baa9fe32bfa910d4
tt23aaz <tt23aaz@herts.ac.uk>
2026-03-22 14:41:44 +0000

    Added snake casing for better readability and changed scaling of the arrow

--- Towers/arrow.gd
+++ Towers/arrow.gd
@@ -2,16 +2,16 @@ extends CharacterBody2D
 
 var target = null
 var Speed = 1000
-var pathName = ""
-var bulletDamage = 5
+var path_name = ""
+var bullet_damage = 5
 
 func _physics_process(delta: float) -> void:
-	var pathSpawnerNode = get_tree().get_root().get_node("Main/PathSpawner")
+	var path_spawner_node = get_tree().get_root().get_node("Main/PathSpawner")
 	target = null
 	
-	for i in range(pathSpawnerNode.get_child_count()):
-		if pathSpawnerNode.get_child(i).name == pathName:
-			target = pathSpawnerNode.get_child(i).get_child(0).get_child(0).global_position
+	for i in range(path_spawner_node.get_child_count()):
+		if path_spawner_node.get_child(i).name == path_name:
+			target = path_spawner_node.get_child(i).get_child(0).get_child(0).global_position
 			break
 			
 	if target == null:
@@ -23,6 +23,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if "EnemyArcher" in body.name:
-		body.Health -= bulletDamage
+	if "Enemy" in body.name:
+		body.Health -= bullet_damage
 		queue_free()
