9e8dd444d7e40bdb32cd93219b8e84c63a66db0a
tt23aaz <tt23aaz@herts.ac.uk>
2026-03-23 11:35:40 +0000

    Implementing path progress and the death handling

--- mobs/enemy_ninja.gd
+++ mobs/enemy_ninja.gd
@@ -4,21 +4,22 @@ extends CharacterBody2D
 var Health = 10
 
 func _process(delta: float) -> void:
-	get_parent().set_progress(get_parent().get_progress() + speed*delta)
-	
-	if get_parent().get_progress_ratio() == 1:
+	get_parent().set_progress(get_parent().get_progress() + speed * delta)
+
+	if get_parent().get_progress_ratio() >= 1.0:
 		Game.Health -= 1
-		death()
-		
+		get_tree().current_scene.get_node("PathSpawner").enemy_removed()
+
 		if Game.Health <= 0:
 			get_tree().change_scene_to_file("res://lose_menu.tscn")
 			return
-		
-		
+
+		death()
+
 	if Health <= 0:
 		Game.Gold += 5
+		get_tree().current_scene.get_node("PathSpawner").enemy_removed()
 		death()
-	
 
 func death():
 	get_parent().get_parent().queue_free()
