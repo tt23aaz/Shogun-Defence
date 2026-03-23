6bb0958c9dac385d87979ee989c0ded1845ed3f4
tt23aaz <tt23aaz@herts.ac.uk>
2026-03-23 11:38:09 +0000

    Updated global game file for wave implementation and tower placement
    For the wave implementation it will say what wave it is, how many waves in that map, and the enemies counted in that wave and says it for each wave from 1-10 and for the tower placement its when in the UI when the wave is completed you cannot drag and drop the tower.

--- Global/Game.gd
+++ Global/Game.gd
@@ -2,7 +2,15 @@ extends Node
 
 var Gold = 100
 var Health = 10
+var current_wave = 0
+var total_waves = 10
+var enemy_count = 0
+var can_place_towers = true
 
 func reset_stats() -> void:
 	Health = 10
 	Gold = 100
+	current_wave = 0
+	total_waves = 10
+	enemy_count = 0
+	can_place_towers = true
